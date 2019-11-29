af3e6aae4a9592284b42cc833862858e8e03368e
xqd915 <1751782099@qq.com>
2019-11-29 01:44:32 -0800

    33

--- doccano/app/app/settings.py
+++ doccano/app/app/settings.py
@@ -233,9 +233,9 @@ REST_FRAMEWORK = {
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
